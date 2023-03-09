af1a19f2153fbe15fb7d66747132911a42daa333
Matthew Remmel <matt.remmel@gmail.com>
2023-03-09 09:35:59 -0500

    Fix todo-comments not highlighting in first opened buffer. Added additional keybinds

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -3,10 +3,13 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
+    event = { 'BufEnter', 'BufNewFile' },
     cmd = {'TodoTelescope', 'TodoTrouble'},
     keys = {
         {'<leader>ft', '<cmd>TodoTelescope<cr>', desc = "Find TODO Comments"},
         {'<leader>xt', '<cmd>TodoTrouble<cr>', desc = "Toggle Trouble for TODO Comments"},
+        { "]t", function() require("todo-comments").jump_next() end, desc = "Next TODO Comment" },
+        { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous TODO Comment" },
     },
     opts = {}
 }
