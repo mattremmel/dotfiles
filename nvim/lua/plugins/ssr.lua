9fe01de0e4e3e5e3ec8e5aaa5b224c9bf37e5505
Matthew Remmel <matt.remmel@gmail.com>
2023-03-13 10:13:01 -0400

    Add structural search and replace

---
+++ nvim/lua/plugins/ssr.lua
@@ -0,0 +1,8 @@
+return {
+    'cshuaimin/ssr.nvim',
+    keys = {
+        {'<leader>rR', '<cmd>lua require("ssr").open()<cr>', desc = 'Structural Replace'},
+        {'<leader>rR', '<cmd>lua require("ssr").open()<cr>', mode = 'v', desc = 'Structural Replace'},
+    },
+    opts = {}
+}
