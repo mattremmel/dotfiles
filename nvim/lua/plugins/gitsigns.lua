ecffdb521fb9297ef6ed1b498d176a1bb3696ab7
Matthew Remmel <matt.remmel@gmail.com>
2023-03-14 21:48:30 -0400

    Add keymap for toggling deleted text. Clean up descriptions

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,21 +3,22 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     cmd = { 'Gitsigns' },
     keys = {
-        {'<leader>gb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Toggle Git Blame'},
+        {'<leader>gb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Toggle Blame'},
+        {'<leader>gd', '<cmd>Gitsigns toggle_deleted<cr>', desc = 'Toggle Deleted'},
         {'<leader>gn', function()
             if vim.wo.diff then return ']c' end
             vim.schedule(function() require('gitsigns').next_hunk() end)
             return '<Ignore>'
         end,
         expr = true,
-        desc = 'Next Git Hunk'},
+        desc = 'Next Hunk'},
         {'<leader>gp', function()
             if vim.wo.diff then return '[c' end
             vim.schedule(function() require('gitsigns').prev_hunk() end)
             return '<Ignore>'
         end,
         expr = true,
-        desc = 'Previous Git Hunk'},
+        desc = 'Previous Hunk'},
     },
     opts = {}
 }
