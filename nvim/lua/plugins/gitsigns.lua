4e9745fac3f83e2cdb4bc05aac09a1f58b707b87
Matthew Remmel <matt.remmel@gmail.com>
2023-03-14 21:25:25 -0400

    Add keymap to toggle git blame virtual text

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,9 @@
 return {
     'lewis6991/gitsigns.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
+    cmd = { 'Gitsigns' },
+    keys = {
+        {'<leader>gb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Toggle Git Blame'}
+    },
     opts = {}
 }
