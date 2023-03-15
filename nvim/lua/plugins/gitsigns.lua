f0a0c601d8d640949995c9919e3cf6afb99f1b51
Matthew Remmel <matt.remmel@gmail.com>
2023-03-14 21:44:15 -0400

    Add keymappings to jump to next/prev git hunk

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,7 +3,21 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     cmd = { 'Gitsigns' },
     keys = {
-        {'<leader>gb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Toggle Git Blame'}
+        {'<leader>gb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Toggle Git Blame'},
+        {'<leader>gn', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() require('gitsigns').next_hunk() end)
+            return '<Ignore>'
+        end,
+        expr = true,
+        desc = 'Next Git Hunk'},
+        {'<leader>gp', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() require('gitsigns').prev_hunk() end)
+            return '<Ignore>'
+        end,
+        expr = true,
+        desc = 'Previous Git Hunk'},
     },
     opts = {}
 }
