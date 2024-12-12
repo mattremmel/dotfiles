9fad0ffea0333f5092aa3c84f9b66be486410d12
Matthew Remmel <matt.remmel@gmail.com>
2024-12-11 22:46:21 -0500

    Add gitsigns back

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,38 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = { "Gitsigns" },
+    keys = {
+        { "<leader>gD", "<cmd>Gitsigns toggle_deleted<cr>", desc = "Toggle Deleted" },
+        {
+            "<leader>gn",
+            function()
+                if vim.wo.diff then
+                    return "]c"
+                end
+                vim.schedule(function()
+                    require("gitsigns").next_hunk()
+                end)
+                return "<Ignore>"
+            end,
+            expr = true,
+            desc = "Next Hunk",
+        },
+        {
+            "<leader>gp",
+            function()
+                if vim.wo.diff then
+                    return "[c"
+                end
+                vim.schedule(function()
+                    require("gitsigns").prev_hunk()
+                end)
+                return "<Ignore>"
+            end,
+            expr = true,
+            desc = "Previous Hunk",
+        },
+        { "ih", ":<C-U>Gitsigns select_hunk<cr>", mode = { "o", "x" }, desc = "Select Hunk" },
+    },
+    opts = {},
+}
